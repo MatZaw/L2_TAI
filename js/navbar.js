cdd3faa478035de352dad5ea8992645edf80e968
MatZaw <45851108+MatZaw@users.noreply.github.com>
2021-03-08 09:25:32 +0100

    added menu

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+let nav = '<ul class="navbar-list">'+
+    '<li><a href="index.html">Home</a></li>'+
+    '<li><a href="contact.html">Kontakt</a></li>'+
+    '</ul>';
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
